b1e8528b898610f55948a16a9d189a504f8012ce
mrunalshri <mrunalyetekar@gmail.com>
2023-03-14 22:20:23 +0000

    completed Activity 3: added CatCard component

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,9 @@ function App(): JSX.Element {
       <Header />
 
       <main>
-        <div className="cards__wrapper"></div>
+        <div className="cards__wrapper">
+          <CatCard />
+        </div>
       </main>
 
       <Footer />
