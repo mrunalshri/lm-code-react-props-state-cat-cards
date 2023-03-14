7d2ff123df4fe1b3326f855e9234e7776c03f62e
mrunalshri <mrunalyetekar@gmail.com>
2023-03-14 23:28:10 +0000

    completed Activity 5: passed props in CatCard

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,9 +1,16 @@
-const CatCard: React.FC = () => (
+interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}
+
+const CatCard: React.FC<CatCardProps> = (props: CatCardProps) => (
   <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
+    <h3 className="card__text card__header">{props.name}</h3>
+    <p className="card__text">Species: {props.species}</p>
+    <p className="card__text">Favourite Food(s): {props.favFoods.toString()}</p>
+    <p className="card__text">Birth Year: {props.birthYear}</p>
   </div>
 );
 
