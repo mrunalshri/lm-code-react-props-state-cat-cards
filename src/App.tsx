6933a4467ca8ed298ac40f1d6e96e1dc77f07f0c
mrunalshri <mrunalyetekar@gmail.com>
2023-03-14 22:31:11 +0000

    completed activity 4: created loop to render CatCard

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ function App(): JSX.Element {
       birthYear: 2021,
     },
   ]);
-  console.log("Our pretties 😻: ", cats);
+
   return (
     <>
       <Navbar />
@@ -90,7 +90,9 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          <CatCard />
+          {cats.map((cat) => (
+            <CatCard />
+          ))}
         </div>
       </main>
 
